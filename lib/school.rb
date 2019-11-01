88288b8450a44a5a881e1e78483f9d3dac4d237a
bahayg <bahayg@gmail.com>
2019-10-31 17:24:50 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,15 @@
 # code here!
 class School
-    attr_reader :name
-    attr_accessor :roster 
+    attr_accessor :name, :roster 
 
     def initialize(name)
         @name = name
-    end
-
-    def roster
         @roster = {}
     end
 
     def add_student(student_name, grade)
         roster[] = []
-        roster[student_name] << grade
+        roster[grade] << student_name
     end
 
     def grade(grade)
