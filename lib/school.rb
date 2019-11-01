918632dcb02ea16c5d65bd08ce38bcbbfb97eb52
bahayg <bahayg@gmail.com>
2019-10-31 17:22:41 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_reader :name
+    attr_accessor :roster 
+
+    def initialize(name)
+        @name = name
+    end
+
+    def roster
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[] = []
+        roster[student_name] << grade
+    end
+
+    def grade(grade)
+    
+
+end
+
+# school = School.new("Bayside High School")
+# school.roster = {}
